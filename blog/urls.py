0540946b30d64962887344ad9e2fc7477e6cdfca
jackalope22 <akers.michael.g@gmail.com>
2020-04-02 11:41:25 -0400

    files needed for heroku

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.conf import settings
+from django.urls import path
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+
+urlpatterns = [
+    path('reference/', views.ReferenceView.as_view(), name='reference'),
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
