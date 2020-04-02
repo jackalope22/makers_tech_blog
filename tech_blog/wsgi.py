5ddaf790e526728a7aff28ecd94f99399b0acfbc
jackalope22 <akers.michael.g@gmail.com>
2020-04-02 15:00:00 -0400

    had to make changes for whitenoise version

--- tech_blog/wsgi.py
+++ tech_blog/wsgi.py
@@ -6,6 +6,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tech_blog.settings')
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
