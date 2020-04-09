f681aeb88ab380fe54dcf8f23aed6bce8b79f500
jackalope22 <akers.michael.g@gmail.com>
2020-04-09 11:21:53 -0400

    missed an s in views

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 urlpatterns = [
-    path('about/', view.AboutView.as_view(), name='about'),
+    path('about/', views.AboutView.as_view(), name='about'),
     path('reference/', views.ReferenceView.as_view(), name='reference'),
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
